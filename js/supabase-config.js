a5928918a414e0d3b91ee57e291713d1d58efd3b
João Henrique Martins Jantorne <joaohenriquemartinsjantorne@gmail.com>
2026-08-28 10:38:21 -0300

    Corrigindo URL base do Supabase e credenciais

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
 // Preencha estes valores com os dados de Project Settings > API no Supabase.
 window.SUPABASE_CONFIG = {
-  url: 'https://kdjxwxiqgwnyzzgdyznq.supabase.co/rest/v1/',
+  url: 'https://kdjxwxiqgwnyzzgdyznq.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtkanh3eGlxZ3dueXp6Z2R5em5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4ODgwNjMsImV4cCI6MjEwMzQ2NDA2M30.5Zeu2mbEi1oPtiTP8vWSHTODjYuB2N7Dl1aLy38axsQ'
 };
